41c705e9bb80ca60bf15ff19fd50c7d848572cc8
benbktech <bbrucher@gmail.com>
2024-02-13 14:59:12 +0100

    Added: Projet in 2. Code

--- 2. Code/Projet/README.md
+++ 2. Code/Projet/README.md
@@ -1,66 +1,75 @@
-## Foundry
+# Introduction
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+Uniswap déploie plusieurs contrats Pool, chacun étant un marché d'échange d'une paire de jetons. Uniswap regroupe tous ses contrats en deux catégories :
 
-Foundry consists of:
+- contrats de base (core),
+- et les contrats de périphérie.
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+Les contrats de base sont, comme leur nom l'indique, des contrats qui implémentent la logique de base. Il s’agit de contrats minimes, peu conviviaux et de bas niveau. Leur objectif est de faire une chose et de la faire de la manière la plus fiable et sécurisée possible. Dans Uniswap V3, il existe 2 contrats de ce type :
 
-## Documentation
+- Contrat de pool, qui met en œuvre la logique fondamentale d’un échange décentralisé.
+- Contrat de factory, qui sert de registre des contrats de pool et de contrat facilitant le déploiement des pools.
 
-https://book.getfoundry.sh/
+## Pool
 
-## Usage
+Pensons aux données que le contrat stockera :
 
-### Build
+- Étant donné que chaque contrat de pool est un marché d'échange de deux jetons, nous devons suivre les adresses des deux jetons. Ces adresses seront statiques, définies une fois pour toutes lors du déploiement du pool (elles seront donc immuables).
+- Chaque contrat de pool est un ensemble de positions de liquidité. Nous les stockerons dans un mappage, où les clés sont des identifiants de position uniques et les valeurs sont des structures contenant des informations sur les positions.
+- Chaque contrat de pool devra également maintenir un registre de ticks – il s'agira d'un mappage avec des clés étant des index de ticks et des valeurs étant des structures stockant des informations sur les ticks.
+- Étant donné que la plage de ticks est limitée, nous devons stocker les limites dans le contrat, sous forme de constantes.
+- Rappelons que les contrats de pool stockent le montant de liquidité, "L". Nous aurons donc besoin d'une variable pour cela.
+- Enfin, nous devons suivre le prix actuel et le tick associé. Nous les stockerons dans un emplacement de stockage pour optimiser la consommation de gaz : ces variables seront souvent lues et écrites ensemble, il est donc logique de bénéficier de la fonctionnalité de regroupement des variables d'état de Solidity.
 
-```shell
-$ forge build
-```
+## Le Code 
 
-### Test
+```solidity
+// src/lib/Tick.sol
+library Tick {
+    struct Info {
+        bool initialized;
+        uint128 liquidity;
+    }
+    ...
+}
 
-```shell
-$ forge test
-```
+// src/lib/Position.sol
+library Position {
+    struct Info {
+        uint128 liquidity;
+    }
+    ...
+}
 
-### Format
+// src/UniswapV3Pool.sol
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
 
-```shell
-$ forge fmt
-```
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
 
-### Gas Snapshots
+    // Pool tokens, immutable
+    address public immutable token0;
+    address public immutable token1;
 
-```shell
-$ forge snapshot
-```
+    // Packing variables that are read together
+    struct Slot0 {
+        // Current sqrt(P)
+        uint160 sqrtPriceX96;
+        // Current tick
+        int24 tick;
+    }
+    Slot0 public slot0;
 
-### Anvil
+    // Amount of liquidity, L.
+    uint128 public liquidity;
 
-```shell
-$ anvil
-```
+    // Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    // Positions info
+    mapping(bytes32 => Position.Info) public positions;
 
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
+    ...
 ```
---
+++ 2. Code/Projet/src/UniswapV3Pool.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.14;
+
+import "./interfaces/IERC20.sol";
+import "./interfaces/IUniswapV3MintCallback.sol";
+import "./interfaces/IUniswapV3SwapCallback.sol";
+
+import "./lib/Position.sol";
+import "./lib/Tick.sol";
+
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    error InsufficientInputAmount();
+    error InvalidTickRange();
+    error ZeroLiquidity();
+
+    event Mint(
+        address sender,
+        address indexed owner,
+        int24 indexed tickLower,
+        int24 indexed tickUpper,
+        uint128 amount,
+        uint256 amount0,
+        uint256 amount1
+    );
+
+    event Swap(
+        address indexed sender,
+        address indexed recipient,
+        int256 amount0,
+        int256 amount1,
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        int24 tick
+    );
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    // Pool tokens, immutable
+    address public immutable token0;
+    address public immutable token1;
+
+    // First slot will contain essential data
+    struct Slot0 {
+        // Current sqrt(P)
+        uint160 sqrtPriceX96;
+        // Current tick
+        int24 tick;
+    }
+
+    struct CallbackData {
+        address token0;
+        address token1;
+        address payer;
+    }
+
+    Slot0 public slot0;
+
+    // Amount of liquidity, L.
+    uint128 public liquidity;
+
+    // Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    // Positions info
+    mapping(bytes32 => Position.Info) public positions;
+
+    constructor(
+        address token0_,
+        address token1_,
+        uint160 sqrtPriceX96,
+        int24 tick
+    ) {
+        token0 = token0_;
+        token1 = token1_;
+
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
+    }
+
+    function mint(
+        address owner,
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount,
+        bytes calldata data
+    ) external returns (uint256 amount0, uint256 amount1) {
+        if (
+            lowerTick >= upperTick ||
+            lowerTick < MIN_TICK ||
+            upperTick > MAX_TICK
+        ) revert InvalidTickRange();
+
+        if (amount == 0) revert ZeroLiquidity();
+
+        ticks.update(lowerTick, amount);
+        ticks.update(upperTick, amount);
+
+        Position.Info storage position = positions.get(
+            owner,
+            lowerTick,
+            upperTick
+        );
+        position.update(amount);
+
+        amount0 = 0.998976618347425280 ether; // TODO: replace with calculation
+        amount1 = 5000 ether; // TODO: replace with calculation
+
+        liquidity += uint128(amount);
+
+        uint256 balance0Before;
+        uint256 balance1Before;
+        if (amount0 > 0) balance0Before = balance0();
+        if (amount1 > 0) balance1Before = balance1();
+        IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(
+            amount0,
+            amount1,
+            data
+        );
+        if (amount0 > 0 && balance0Before + amount0 > balance0())
+            revert InsufficientInputAmount();
+        if (amount1 > 0 && balance1Before + amount1 > balance1())
+            revert InsufficientInputAmount();
+
+        emit Mint(
+            msg.sender,
+            owner,
+            lowerTick,
+            upperTick,
+            amount,
+            amount0,
+            amount1
+        );
+    }
+
+    function swap(address recipient, bytes calldata data)
+        public
+        returns (int256 amount0, int256 amount1)
+    {
+        int24 nextTick = 85184;
+        uint160 nextPrice = 5604469350942327889444743441197;
+
+        amount0 = -0.008396714242162444 ether;
+        amount1 = 42 ether;
+
+        (slot0.tick, slot0.sqrtPriceX96) = (nextTick, nextPrice);
+
+        IERC20(token0).transfer(recipient, uint256(-amount0));
+
+        uint256 balance1Before = balance1();
+        IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(
+            amount0,
+            amount1,
+            data
+        );
+        if (balance1Before + uint256(amount1) > balance1())
+            revert InsufficientInputAmount();
+
+        emit Swap(
+            msg.sender,
+            recipient,
+            amount0,
+            amount1,
+            slot0.sqrtPriceX96,
+            liquidity,
+            slot0.tick
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // INTERNAL
+    //
+    ////////////////////////////////////////////////////////////////////////////
+    function balance0() internal returns (uint256 balance) {
+        balance = IERC20(token0).balanceOf(address(this));
+    }
+
+    function balance1() internal returns (uint256 balance) {
+        balance = IERC20(token1).balanceOf(address(this));
+    }
+}
